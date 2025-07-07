4aae4c98f73923d943ef87c9b5f67e99de7bce60
Tom de Roos <je.email@example.com>
2025-07-07 22:05:51 +0200

    Update base-path voor nieuwe repo naam wie-is-die-renner

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rugnummerzoeker/',
+  base: '/wie-is-die-renner/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
